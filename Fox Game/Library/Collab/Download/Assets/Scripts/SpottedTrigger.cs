c07e49915897ab52c613ebd6d989b03e8185fa8e
TyrdleDev <44824998+TyrdleDev@users.noreply.github.com>
2019-09-06 16:56:32 +0800

    merge update

---
+++ Fox Game/Library/Collab/Download/Assets/Scripts/SpottedTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class SpottedTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Fox Game/Library/Collab/Original/Assets/Scripts/CageManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CageManager : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GetComponentInParent<Animator>().SetTrigger("triggerCage");
+            GetComponentInChildren<FollowMovement>().isFollowing = true;
+        }
+    }
+}
